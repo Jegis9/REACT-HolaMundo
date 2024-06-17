8c1217b51424b04a21ad96a76cce848ec552f87c
Jegis9 <ixcaquic22@gmailcom>
2024-06-16 21:33:02 -0600

    Gestion de eventos

--- react-app/src/components/List.tsx
+++ react-app/src/components/List.tsx
@@ -3,10 +3,23 @@ type Props = {
 };
 
 export default function List({ data }: Props) {
+  // SE CREA UNA FUNCION PARA PODER ENVIAR EL PARAMETRO
+  const handleClick = (e: string) => {
+    console.log(e);
+  };
+
   return (
     <ul className="list-group">
       {data.map((element) => (
-        <li className="list-group-item">{element}</li>
+        <li
+          //   SE LLAMA A LA FUNCION Y SE LE ENVIA COMO PARAMETRO EL ELEMENTO DE LA LISTA
+          //   PARA MOSTRAR CUANDO SE DE CLIC
+          onClick={() => handleClick(element)}
+          key={element}
+          className="list-group-item"
+        >
+          {element}
+        </li>
       ))}
     </ul>
   );
