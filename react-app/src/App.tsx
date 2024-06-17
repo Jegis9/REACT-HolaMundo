3fd30a72f6858347a8885a2ba188a4117a49b6b0
Jegis9 <ixcaquic22@gmailcom>
2024-06-17 14:48:41 -0600

    Renderizado condicional

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,29 +1,24 @@
-//IMPORTAR ARCHIVO TITULO
-
 import Card, { CardBody } from "./components/Card";
 import List from "./components/List";
 
 function App() {
   const list: string[] = [];
 
-  //SE CREA LA FUNCION HANDLESELECT PARA IMPRIMIR FUNCION
   const handleSelect = (Element: string) => {
     console.log("Imprimiendo", Element);
   };
-  //CADA UNA ES INDEPENDIENTE DE CADA FUNCION QUE SE LLAMA
-  //SE CREA LA FUNCION HANDLESELECT PARA IMPRIMIR FUNCION
-  const handleSelect2 = (Element: string) => {
-    console.log("Mostrando", Element);
-  };
+
+  //SE CREA UNA CONSTANTE EN DONDE SE CREA LA CONDICIONAL+
+  //SI LA LISTA ES VACIA NO SE MUESTRA NADA, SI ES LLENA, SE MUESTRA EL CONTENIDO
+  const contenido = list.length !== 0 && (
+    <List data={list} onSelect={handleSelect} />
+  );
+
   return (
     <Card>
-      {/* PRIMERO SE TIENE QUE EVALUAR SI LA LISTA ES DIFERENTE A 0 ENTONCES MUESTRA LA LISTA
-      SI SE DEJA SIN LA CONDICIONAL DIFERENTE SE MOSTRARA EL 0 EL CUAL NO DEBE DE APARECER */}
       {list.length !== 0 && "Lista llena"}
       <CardBody title="Hola mundo soy el titulo" text="Soy el contenido" />
-
-      <List data={list} onSelect={handleSelect} />
-      <List data={list} onSelect={handleSelect2} />
+      {contenido}
     </Card>
   );
 }
